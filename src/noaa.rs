045430bdf53470b1ea08a13f3cbb9cdd3d5e44b9
Matthew Scheffel <mscheffel@gmail.com>
2020-07-29 15:25:01 -0400

    NOAA support implemented!

---
+++ src/noaa.rs
@@ -0,0 +1,317 @@
+extern crate ftp;
+
+use std::fmt;
+use std::io::{Read, Cursor};
+use std::convert::TryInto;
+use std::result;
+
+use fixed_width::{Reader, FixedWidth, Field, LineBreak};
+use flate2::read::GzDecoder;
+use ftp::FtpStream;
+use ftp::types::FileType::Binary;
+use tar::Archive;
+
+use serde::{Deserialize, Deserializer};
+use serde::de::Error;
+
+pub enum Element {
+    Precipitation,  // PRCP, tenths of mm
+    Snowfall,       // SNOW (mm)
+    SnowDepth,      // SNWD (mm)
+    MaxTemp,        // TMAX (tenths of degrees C)
+    MinTemp,        // TMIN (tenths of degrees C)
+    AverageCloudinessM2MCeilometer,     // ACMC, %
+    AverageCloudinessM2MManual,         // ACMH, %
+    AverageCloudinessS2SCeilometer,     // ACSC, %
+    AverageCloudinessS2SManual,         // ACSH, %
+}
+
+#[derive(Debug)]
+pub enum MeasurementFlag {
+    PrecipitationTotalFromTwoTwelveHourTotals,
+    PrecipitationTotalFromFourSixHourTotals,
+    HourlyPoint, // either highest in an hour (TMIN), lowest in an hour (TMAX), or average over an hour (TAVG)
+    ConvertedFromKnots,
+    TemperatureLaggedFromObservation,
+    ConvertedFromOktas,
+    MissingPresumedZero,
+    TraceOfPrecipitation, // or snow fall, or snow depth
+    ConvertedFromWBANCode,
+}
+impl<'de> Deserialize<'de> for MeasurementFlag {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de> {
+            let s = String::deserialize(deserializer)?;
+
+            match s.as_ref() {
+                "B" => {Ok(MeasurementFlag::PrecipitationTotalFromTwoTwelveHourTotals)},
+                "D" => {Ok(MeasurementFlag::PrecipitationTotalFromFourSixHourTotals)},
+                "H" => {Ok(MeasurementFlag::HourlyPoint)},
+                "K" => {Ok(MeasurementFlag::ConvertedFromKnots)},
+                "L" => {Ok(MeasurementFlag::TemperatureLaggedFromObservation)},
+                "O" => {Ok(MeasurementFlag::ConvertedFromOktas)},
+                "P" => {Ok(MeasurementFlag::MissingPresumedZero)},
+                "T" => {Ok(MeasurementFlag::TraceOfPrecipitation)},
+                "W" => {Ok(MeasurementFlag::ConvertedFromWBANCode)},
+                q => {Err(D::Error::custom(String::from(format!("Unknown measurement flag: {}", q))))}
+            }
+        }
+}
+
+
+#[derive(Debug)]
+pub enum QualityFlag {
+    Duplicate,              // D
+    Gap,                    // G
+    InternalConsistency,    // I
+    StreakFrequent,         // K
+    Length,                 // L
+    Megaconsistency,        // M
+    Naught,                 // N
+    ClimatologicalOutlier,  // O
+    LaggedRange,            // R
+    SpatialConsistency,     // S
+    TemporalConsistency,    // T
+    TooWarmForSnow,         // W
+    FailedBoundsCheck,      // X
+    FlaggedDatzilla,        // Z
+}
+
+impl<'de> Deserialize<'de> for QualityFlag {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de> {
+            let s = String::deserialize(deserializer)?;
+
+            match s.as_ref() {
+                "D" => {Ok(QualityFlag::Duplicate)},
+                "G" => {Ok(QualityFlag::Gap)},
+                "I" => {Ok(QualityFlag::InternalConsistency)},
+                "K" => {Ok(QualityFlag::StreakFrequent)},
+                "L" => {Ok(QualityFlag::Length)},
+                "M" => {Ok(QualityFlag::Megaconsistency)},
+                "N" => {Ok(QualityFlag::Naught)},
+                "O" => {Ok(QualityFlag::ClimatologicalOutlier)},
+                "R" => {Ok(QualityFlag::LaggedRange)},
+                "S" => {Ok(QualityFlag::SpatialConsistency)},
+                "T" => {Ok(QualityFlag::TemporalConsistency)},
+                "W" => {Ok(QualityFlag::TooWarmForSnow)},
+                "X" => {Ok(QualityFlag::FailedBoundsCheck)},
+                "Z" => {Ok(QualityFlag::FlaggedDatzilla)},
+                q => {Err(D::Error::custom(String::from(format!("Unknown quality flag: {}", q))))}
+            }
+        }
+}
+
+fn value_process<'de, D>(deserializer: D) -> Result<Option<isize>, D::Error> 
+    where D: Deserializer<'de> {
+    let input = isize::deserialize(deserializer)?;
+
+    if input == -9999 {
+        Ok(None)
+    } else {
+        Ok(Some(input))
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DailyObservation {
+    #[serde(deserialize_with = "value_process")]
+    pub value: Option<isize>,
+    pub measure_flag: Option<MeasurementFlag>,
+    pub quality_flag: Option<QualityFlag>,
+    pub source_flag: String
+}
+
+impl fmt::Display for DailyObservation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?} (M={:?}, Q={:?}, S={:?})", self.value, self.measure_flag, self.quality_flag, self.source_flag)
+    }
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct Observation {
+    pub station_id: String,
+    pub year: usize,
+    pub month: usize,
+    pub element: String,
+    pub observations: Vec<DailyObservation>
+}
+
+impl FixedWidth for Observation {
+    fn fields() -> Vec<Field> {
+        let mut field_vec = vec![
+            Field::default().range(0..11),
+            Field::default().range(11..15),
+            Field::default().range(15..17),
+            Field::default().range(17..21)
+        ];
+
+        let mut index = 21;
+        for _ in 0..31 {
+            field_vec.push(Field::default().range(index..index+5)); // value
+            field_vec.push(Field::default().range(index+5..index+6)); // m flag
+            field_vec.push(Field::default().range(index+6..index+7)); // q flag
+            field_vec.push(Field::default().range(index+7..index+8)); // s flag
+            index += 8;
+        }
+
+        field_vec
+    }
+}
+
+impl fmt::Display for Observation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Station ID: '{}'. {}-{:02}. Element: {}\n", self.station_id, self.year, self.month, self.element);
+        write!(f, "Records:\n");
+        for i in 0..31 {
+            let day = self.observations.get(i).unwrap();
+            write!(f, "{} = {}\n", i+1, day);
+        }
+        Ok(())
+    }
+}
+
+pub fn retrieve_noaa_ftp() -> Result<Cursor<Vec<u8>>, String> {
+    let mut ftp_stream = {
+        match FtpStream::connect("ftp.ncdc.noaa.gov:21") {
+            Ok(stream) => { stream },
+            Err(e) => {
+                return Err(e.to_string())
+            }
+        }
+    };
+
+    match ftp_stream.login("anonymous", "matt@dataheck.com") {
+        Ok(_) => {},
+        Err(e) => {
+            return Err(e.to_string())
+        }
+    }
+
+    match ftp_stream.transfer_type(Binary) {
+        Ok(_) => {},
+        Err(e) => {
+            return Err(String::from(format!("Failed to set transfer type to binary: {}", e)))
+        }
+    }
+
+    let cursor = { 
+        match ftp_stream.simple_retr("/pub/data/ghcn/daily/ghcnd_gsn.tar.gz") {
+            Ok(stream) => { stream },
+            Err(e) => {
+                return Err(String::from(format!("Failed to read stream: {}", e)))
+            }
+        }
+    };
+
+    Ok(cursor)
+}
+
+pub fn process_noaa<R: Read>(cursor: R, element_filter: Option<String>) -> Result<Vec<Observation>, String> {   
+    let tar = GzDecoder::new(cursor);
+    match tar.header() {
+        Some(_) => {},
+        None => {
+            return Err(String::from("Gzip header is not valid"))
+        }
+    }
+
+    let mut archive = Archive::new(tar);
+
+    let entries = match archive.entries() {
+        Ok(result) => { result },
+        Err(_) => { return Err(String::from("Failed to read archive from NOAA")) }
+    };
+
+    let mut results = Vec::new();
+    for file in entries {
+        let mut file = match file {
+            Ok(f) => {f},
+            Err(_) => {return Err(String::from("Failed to read file in archive from NOAA"))}
+        };
+
+        println!("{:?}", file.path());
+
+        let file_size: usize = match file.header().size() {
+            Ok(s) => {
+                match s.try_into() {
+                    Ok(s) => { s },
+                    Err(_) => {
+                        return Err(String::from("File in archive too large, unable to process."))
+                    }
+                }
+            },
+            Err(_) => {
+                return Err(String::from("Failed to identify size of file in archive from NOAA"))
+            }
+        };
+
+        let mut buffer = Vec::with_capacity(file_size);
+        match file.read_to_end(&mut buffer) {
+            Ok(_) => {},
+            Err(e) => {return Err(String::from(format!("Failed to read file in archive into memory: {}", e)))}
+        }
+
+        let mut reader = Reader::from_bytes(buffer).width(269).linebreak(LineBreak::Newline);
+        
+        for row in reader.byte_reader().filter_map(result::Result::ok) {
+            let record_result: Result<Observation, _> = fixed_width::from_bytes(&row);
+
+            match record_result {
+                Ok(record) => {
+                    match element_filter.as_ref() {
+                        Some(filter) => {
+                            if *filter == record.element {
+                                results.push(record);
+                            }
+                        },
+                        None => {
+                            results.push(record);
+                        }
+                    }
+                },
+                Err(e) => {
+                    println!("error: {}", e)
+                }
+            }
+        }
+    }
+
+    Ok(results)
+}
+
+#[test]
+fn test_process_noaa() {
+    use tar::{Builder, Header};
+    use flate2::write::GzEncoder;
+    use flate2::Compression;
+    use std::io::prelude::*;
+
+    let test_string = r#"AE000041196194403TAVG-9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999   -9999     292H S  274H S  242H S  250H S  263H S  257H S  233H S  239H S  217H S  245H S  292H S  260H S
+AE000041196194404TMAX  258  I  263  I  258  I  263  I  296  I  302  I  358  I  391  I  380  I  308  I  291  I  274  I  280  I  369  I  330  I  335  I  385  I  385  I  374  I  374  I  313  I  308  I  308  I  302  I  313  I  330  I  335  I  302  I  313  I  346  I-9999   
+AE000041196194404TMIN  180  I  180  I  163  I  146  I  135  I-9999   -9999     196  I  235  I  213  I  163  I-9999     180  I  174  I-9999     196  I  241  I  235  I  208  I  196  I  208  I  213  I  180  I  174  I  180  I  180  I  169  I  152  I  169  I  169  I-9999   
+"#;
+
+    let cursor = Cursor::new(test_string);
+    
+    let mut header = Header::new_gnu();
+    header.set_path("foo.dly").unwrap();
+    header.set_size(test_string.len().try_into().unwrap());
+    header.set_cksum();
+    
+    let mut archive = Builder::new(Vec::new());
+    archive.append(&header, cursor).unwrap();
+    let archive = archive.into_inner().unwrap();
+
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(&archive[..]).unwrap();
+
+    let result = encoder.finish().unwrap();
+    let cursor = Cursor::new(result);
+
+    let results = process_noaa(cursor, Some("TAVG".to_string())).unwrap();
+    for observation in results {
+        println!("{}", observation);
+    }    
+}
